b4a8b24a489dbd685273ef1298266f582e9f2c4c
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-06-23 12:04:44 +0400

    Add AgentController class

---
+++ src/main/java/helpdesk_ticketing_system/controller/AgentController.java
@@ -0,0 +1,10 @@
+package helpdesk_ticketing_system.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/agents")
+public class AgentController {
+
+}
