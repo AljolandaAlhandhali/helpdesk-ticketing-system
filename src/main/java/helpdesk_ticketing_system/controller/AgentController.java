9a7d50faeb30af1e61a6cac9dbe57e5d0d15a8ab
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-06-23 12:05:26 +0400

    Inject AgentService into AgentController

--- src/main/java/helpdesk_ticketing_system/controller/AgentController.java
+++ src/main/java/helpdesk_ticketing_system/controller/AgentController.java
@@ -1,10 +1,16 @@
 package helpdesk_ticketing_system.controller;
 
+import helpdesk_ticketing_system.service.AgentService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/agents")
 public class AgentController {
+    private final AgentService agentService;
+
+    public AgentController(AgentService agentService) {
+        this.agentService = agentService;
+    }
 
 }
