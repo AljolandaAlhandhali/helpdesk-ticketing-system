95684d853f0a16a180e84db950d0f9edfedc795e
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-06-23 12:27:00 +0400

    Add global exception handler

--- src/main/java/helpdesk_ticketing_system/exception/GlobalExceptionHandler.java
+++ src/main/java/helpdesk_ticketing_system/exception/GlobalExceptionHandler.java
@@ -1,4 +1,8 @@
 package helpdesk_ticketing_system.exception;
 
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+// Handles all API errors
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 }
