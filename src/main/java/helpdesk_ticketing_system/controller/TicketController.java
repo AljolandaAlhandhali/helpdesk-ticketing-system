42bffd7261ab4959fb2289a8b143eafeff090c26
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-06-23 12:14:30 +0400

    Add create ticket endpoint

--- src/main/java/helpdesk_ticketing_system/controller/TicketController.java
+++ src/main/java/helpdesk_ticketing_system/controller/TicketController.java
@@ -1,8 +1,10 @@
 package helpdesk_ticketing_system.controller;
 
+import helpdesk_ticketing_system.dto.CreateTicketRequest;
+import helpdesk_ticketing_system.model.Ticket;
 import helpdesk_ticketing_system.service.TicketService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/tickets")
@@ -12,5 +14,12 @@ public class TicketController {
 
     public TicketController(TicketService ticketService) {
         this.ticketService = ticketService;
+    }
+
+    // Create ticket
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Ticket createTicket(@RequestBody CreateTicketRequest request) {
+        return ticketService.createTicket(request);
     }
 }
