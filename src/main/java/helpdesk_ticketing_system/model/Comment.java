65248992ab3eb506c6bdbc5e806d66749255bc9c
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-06-23 09:58:56 +0400

    Add comment primary key

--- src/main/java/helpdesk_ticketing_system/model/Comment.java
+++ src/main/java/helpdesk_ticketing_system/model/Comment.java
@@ -1,10 +1,13 @@
 package helpdesk_ticketing_system.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "comments")
 public class Comment {
+    // Primary Key
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
 
 }
