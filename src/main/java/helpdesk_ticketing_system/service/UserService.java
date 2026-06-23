ffb54678d55226084cd4c34a1d06176cc2fe7696
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-06-23 11:44:58 +0400

    Add UserRepository dependency

--- src/main/java/helpdesk_ticketing_system/service/UserService.java
+++ src/main/java/helpdesk_ticketing_system/service/UserService.java
@@ -1,8 +1,9 @@
 package helpdesk_ticketing_system.service;
 
+import helpdesk_ticketing_system.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-
+    private final UserRepository userRepository;
 }
