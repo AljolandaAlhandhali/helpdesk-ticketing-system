b3859cbf8db568f9213be466373a7eddab1920e0
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-06-23 10:32:26 +0400

    Add user primary key

--- src/main/java/helpdesk_ticketing_system/model/User.java
+++ src/main/java/helpdesk_ticketing_system/model/User.java
@@ -1,10 +1,13 @@
 package helpdesk_ticketing_system.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
 
 }
