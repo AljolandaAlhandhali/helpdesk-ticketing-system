d5b135c3ce1d714bea016d11c1067ea5bdc8a1b0
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-06-23 10:08:35 +0400

    Add ticket primary key

--- src/main/java/helpdesk_ticketing_system/model/Ticket.java
+++ src/main/java/helpdesk_ticketing_system/model/Ticket.java
@@ -1,10 +1,12 @@
 package helpdesk_ticketing_system.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "tickets")
 public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ticketId;
 
 }
