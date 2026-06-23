680c967165c42477b6ffc357de2f3b5d0d2df82f
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-06-23 12:13:31 +0400

    Add TicketController class and service injection

---
+++ src/main/java/helpdesk_ticketing_system/controller/TicketController.java
@@ -0,0 +1,16 @@
+package helpdesk_ticketing_system.controller;
+
+import helpdesk_ticketing_system.service.TicketService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/tickets")
+public class TicketController {
+
+    private final TicketService ticketService;
+
+    public TicketController(TicketService ticketService) {
+        this.ticketService = ticketService;
+    }
+}
