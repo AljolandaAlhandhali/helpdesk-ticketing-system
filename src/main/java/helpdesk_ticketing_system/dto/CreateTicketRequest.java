b18835397f67763b542f71af5945dd04dc4229b5
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-06-23 11:06:31 +0400

    Add priority field to CreateTicketRequest

--- src/main/java/helpdesk_ticketing_system/dto/CreateTicketRequest.java
+++ src/main/java/helpdesk_ticketing_system/dto/CreateTicketRequest.java
@@ -1,7 +1,10 @@
 package helpdesk_ticketing_system.dto;
 
+import helpdesk_ticketing_system.enums.Priority;
+
 public class CreateTicketRequest {
     private String title;
     private String description;
+    private Priority priority;
 
 }
