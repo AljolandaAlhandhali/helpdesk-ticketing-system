928d9d0b581eeb403049f75e7577e2d2d4425efd
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-06-23 11:35:55 +0400

    Add getter for status in UpdateStatusRequest

--- src/main/java/helpdesk_ticketing_system/dto/UpdateStatusRequest.java
+++ src/main/java/helpdesk_ticketing_system/dto/UpdateStatusRequest.java
@@ -5,4 +5,8 @@ import helpdesk_ticketing_system.enums.TicketStatus;
 public class UpdateStatusRequest {
     private TicketStatus status;
 
+    public TicketStatus getStatus() {
+        return status;
+    }
+
 }
