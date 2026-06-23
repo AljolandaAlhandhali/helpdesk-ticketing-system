73b256a3d6dac278ab650c7df593f0ff5e4c8ed6
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-06-23 10:24:16 +0400

    Add old ticket status field

--- src/main/java/helpdesk_ticketing_system/model/TicketStatusHistory.java
+++ src/main/java/helpdesk_ticketing_system/model/TicketStatusHistory.java
@@ -1,5 +1,6 @@
 package helpdesk_ticketing_system.model;
 
+import helpdesk_ticketing_system.enums.TicketStatus;
 import jakarta.persistence.*;
 
 @Entity
@@ -9,5 +10,9 @@ public class TicketStatusHistory {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "HISTORY_ID")
     private Long historyId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "OLD_STATUS")
+    private TicketStatus oldStatus;
 
 }
