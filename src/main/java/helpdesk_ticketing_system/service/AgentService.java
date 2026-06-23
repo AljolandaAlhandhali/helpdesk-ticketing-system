a4d0101208a2147a5f5bfda21600b76b4cbb18c5
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-06-23 11:39:04 +0400

    Add AgentRepository dependency

--- src/main/java/helpdesk_ticketing_system/service/AgentService.java
+++ src/main/java/helpdesk_ticketing_system/service/AgentService.java
@@ -1,8 +1,10 @@
 package helpdesk_ticketing_system.service;
 
+import helpdesk_ticketing_system.repository.AgentRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AgentService {
+    private final AgentRepository agentRepository;
 
 }
