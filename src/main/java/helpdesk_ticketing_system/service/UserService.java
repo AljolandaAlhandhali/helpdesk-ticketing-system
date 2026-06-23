847e118f7273bb458e8920bd7097ddbd4657487d
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-06-23 11:45:56 +0400

    Add createUser service method

--- src/main/java/helpdesk_ticketing_system/service/UserService.java
+++ src/main/java/helpdesk_ticketing_system/service/UserService.java
@@ -1,5 +1,7 @@
 package helpdesk_ticketing_system.service;
 
+import helpdesk_ticketing_system.dto.CreateUserRequest;
+import helpdesk_ticketing_system.model.User;
 import helpdesk_ticketing_system.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +11,9 @@ public class UserService {
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public User createUser(CreateUserRequest request) {
+
     }
 }
