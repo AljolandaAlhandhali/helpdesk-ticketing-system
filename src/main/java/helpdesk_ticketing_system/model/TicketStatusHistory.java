d119a27442176bfadb2aa9b79f58d5fc6034757f
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-06-23 10:23:55 +0400

    Add history identifier field

--- src/main/java/helpdesk_ticketing_system/model/TicketStatusHistory.java
+++ src/main/java/helpdesk_ticketing_system/model/TicketStatusHistory.java
@@ -1,10 +1,13 @@
 package helpdesk_ticketing_system.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "TICKET_STATUS_HISTORY")
 public class TicketStatusHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "HISTORY_ID")
+    private Long historyId;
 
 }
